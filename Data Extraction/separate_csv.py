c5e23a68a3a4f6f9c0d23f1d468b18eb3f868e23
Key <ruine_hawk274@hotmail.com>
2020-05-25 22:23:51 -0700

    Initial commit
    Code to separate finance data by year

---
+++ Data Extraction/separate_csv.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 25 19:07:33 2020
+
+@author: Will
+"""
+
+# import libraries
+import pandas_datareader as pdr
+
+# read data from Yahoo Finance
+df_oil = pdr.DataReader('CL=F', data_source='yahoo', start='2000-01-01', end='2020-03-30').reset_index(drop=False)
+
+
+# extract year from 'Date'
+df_oil['year'] = df_oil['Date'].dt.year
+
+# extract csv
+for i in range(2000, 2021):
+    df_oil.loc[df_oil['year'] == i].to_csv(f'df_{i}.csv')
